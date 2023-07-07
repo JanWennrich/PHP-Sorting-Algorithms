12459be7505a5b3d25fe31b49c664ab30eba9476
Jan Wennrich <jan.wennrich@pcsg.de>
2023-07-07 20:49:10 +0200

    feat: add dummy MergeSort class

---
+++ src/PhpSorting/Algorithm/MergeSort.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PhpSorting\Algorithm;
+
+class MergeSort implements SortingAlgorithm
+{
+    public function sort(array $dataset): array
+    {
+        return $dataset;
+    }
+
+}
