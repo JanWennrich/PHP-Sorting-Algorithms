803f627fbccb13bea7846e14238bdb151395bd6a
Jan Wennrich <jan.wennrich@pcsg.de>
2023-07-08 17:45:06 +0200

    feat: implement MergeSort algorithm

--- src/PhpSorting/Algorithm/MergeSort.php
+++ src/PhpSorting/Algorithm/MergeSort.php
@@ -2,11 +2,41 @@
 
 namespace PhpSorting\Algorithm;
 
+use function array_chunk;
+use function array_merge;
+use function array_shift;
+use function count;
+
 class MergeSort implements SortingAlgorithm
 {
     public function sort(array $dataset): array
     {
-        return $dataset;
+        $datasetLength = count($dataset);
+
+        if ($datasetLength <= 1) {
+            return $dataset;
+        }
+
+        $splitDataset = array_chunk($dataset, ceil($datasetLength / 2));
+
+        $leftDataset  = static::sort($splitDataset[0]);
+        $rightDataset = static::sort($splitDataset[1]);
+
+        return static::mergeDatasets($leftDataset, $rightDataset);
     }
 
+    protected function mergeDatasets(array $leftDataset, array $rightDataset): array
+    {
+        $result = [];
+
+        while (!empty($leftDataset) && !empty($rightDataset)) {
+            if ($leftDataset[0] <= $rightDataset[0]) {
+                $result[] = array_shift($leftDataset);
+            } else {
+                $result[] = array_shift($rightDataset);
+            }
+        }
+
+        return array_merge($result, $leftDataset, $rightDataset);
+    }
 }
