ddfe6a9eed5aeccf809d6b51ee9096bb18a35ec7
Jan Wennrich <jan.wennrich@pcsg.de>
2023-07-09 15:28:45 +0200

    feat: implement InsertionSort

--- src/PhpSorting/Algorithm/InsertionSort.php
+++ src/PhpSorting/Algorithm/InsertionSort.php
@@ -2,11 +2,34 @@
 
 namespace PhpSorting\Algorithm;
 
+use function array_shift;
+
 class InsertionSort implements SortingAlgorithm
 {
     public function sort(array $dataset): array
     {
-        return $dataset;
-    }
+        $result = [];
 
+        while ($datasetValue = array_shift($dataset)) {
+            $insertPosition = 0;
+
+            foreach ($result as $position => $resultValue) {
+                if ($resultValue < $datasetValue) {
+                    $insertPosition++;
+                    continue;
+                }
+
+                break;
+            }
+
+            array_splice(
+                $result,
+                $insertPosition,
+                0,
+                [$datasetValue]
+            );
+        }
+
+        return $result;
+    }
 }
