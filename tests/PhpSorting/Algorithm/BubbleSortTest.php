71b4ef3d6398fde6d3ab26a972c27d6e08d5300b
Jan Wennrich <jan.wennrich@pcsg.de>
2023-07-09 16:07:53 +0200

    feat: add BubbleSort

--- tests/PhpSorting/Algorithm/BubbleSortTest.php
+++ tests/PhpSorting/Algorithm/BubbleSortTest.php
@@ -2,13 +2,10 @@
 
 namespace PhpSorting\Algorithm;
 
-use PHPUnit\Framework\TestCase;
-
 class BubbleSortTest extends AbstractSortingAlgorithmTest
 {
     protected function setSortingAlgorithm(): void
     {
         $this->sortingAlgorithm = new BubbleSort();
     }
-
 }
